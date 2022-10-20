d78fdf03369c1a03c42815b92b682e4671e16cdc
Israel Huerta <israelhf24@gmail.com>
2022-10-20 05:37:12 -0500

    Add eurka project

---
+++ springboot-service-eureka-server/src/main/java/com/huerta/springboot/app/eureka/springbootserviceeurekaserver/SpringbootServiceEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.huerta.springboot.app.eureka.springbootserviceeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServiceEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServiceEurekaServerApplication.class, args);
+	}
+
+}
