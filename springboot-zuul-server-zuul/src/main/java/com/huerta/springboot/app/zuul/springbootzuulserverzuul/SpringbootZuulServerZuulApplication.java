aa445628193ff942e375247c8c5c6b3733d13acf
Israel Huerta <israelhf24@gmail.com>
2022-10-26 11:23:25 -0500

    Adding zuul project

---
+++ springboot-zuul-server-zuul/src/main/java/com/huerta/springboot/app/zuul/springbootzuulserverzuul/SpringbootZuulServerZuulApplication.java
@@ -0,0 +1,13 @@
+package com.huerta.springboot.app.zuul.springbootzuulserverzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootZuulServerZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootZuulServerZuulApplication.class, args);
+	}
+
+}
---
+++ springboot-zuul-server-zuul/src/test/java/com/huerta/springboot/app/zuul/springbootzuulserverzuul/SpringbootZuulServerZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huerta.springboot.app.zuul.springbootzuulserverzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootZuulServerZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
